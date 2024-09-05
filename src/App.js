e04845d5d1086368a6099162399cac6240796ad3
soutulkhair <soutulkhairorg@gmail.com>
2024-09-05 09:34:45 +0000

    personalize

--- src/App.js
+++ src/App.js
@@ -10,18 +10,9 @@ class App extends React.Component {
       <div className="card">
         <div className="header">
           <div className="logo">
-            <a href=".">LOGO</a>
+            <a href=".">Soutulkhair</a>
           </div>
           <div className="social">
-            <a href="https://facebook.com" title="Facebook" target="_blank" rel="noopener noreferrer">
-              <IconFacebook className="icon" />
-            </a>
-            <a href="https://twitter.com" title="Twitter" target="_blank" rel="noopener noreferrer">
-              <IconTwitter className="icon" />
-            </a>
-            <a href="https://github.com/arkn98/coming-soon" title="GitHub" target="_blank" rel="noopener noreferrer">
-              <IconGithub className="icon" />
-            </a>
           </div>
         </div>
         <div className="content">
@@ -29,7 +20,7 @@ class App extends React.Component {
             <h1>Get ready for the change.</h1>
             <p>Website coming soon. Please check back to know more. Shoot us an email if you're curious.</p>
           </div>
-          <a href="mailto:user@example.com">
+          <a href="mailto:coming-soon@soutulkhair.com">
             <div className="cta">Send us an email</div>
           </a>
         </div>
